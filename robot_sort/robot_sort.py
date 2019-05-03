fbf3376ca1d4a4955139acdf5829fd515fcbb629
KROHNICK <business.kanekperez@gmail.com>
2019-05-03 08:27:47 -0700

    finished robot sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,8 +96,25 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        self.swap_item()
+
+        while(True==True):
+            self.set_light_off()
+            while(self.can_move_right()==True):
+                self.move_right()
+                if(self.compare_item()==1):
+                    self.swap_item()
+                    self.set_light_on()
+                self.move_left()
+                self.swap_item()
+                self.move_right()
+                self.swap_item()
+            self.swap_item()
+            if(self.light_is_on()==False):
+                break
+            while(self.can_move_left()):
+                self.move_left()
+            self.swap_item()
 
 
 if __name__ == "__main__":
